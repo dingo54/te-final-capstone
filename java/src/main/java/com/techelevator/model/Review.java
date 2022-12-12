26ac55c41b55df02dfc4726482609fbb4955bbad
Ryan Montville <ryan.a.montville>
2022-12-12 11:03:28 -0500

    created review model

---
+++ java/src/main/java/com/techelevator/model/Review.java
@@ -0,0 +1,11 @@
+package com.techelevator.model;
+
+public class Review {
+
+    private int ReviewId;
+    private int BeerId;
+    private int UserId;
+    private int Rating;
+    private String Review;
+
+}
